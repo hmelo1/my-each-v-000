164eca5f749a7b047e2adb4588b2b5db1a3fe81c
Eddie Melo <heriberto@melo.nyc>
2017-12-09 04:17:05 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(array) # put argument(s) here
   # code here
   items = 0
   while items < array.length
-    yield array[i]
-    i = i+1
+    yield array[items]
+    items = items +1
   end
 end
